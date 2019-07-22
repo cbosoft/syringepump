ee3522c6aca5144b0b3a1c32e22d9fc1c4f21a5d
Chris <chrismjboyle@gmail.com>
2019-07-22 14:08:20 +0100

    Trimmed

--- src/main.c
+++ src/main.c
@@ -1,45 +1,18 @@
 #include <gtk/gtk.h>
 #include <pthread.h>
 
-static int THREAD_STOP = 0;
-
-void *f(void * void_lbl)
-{
-
-  GObject *lbl = (GObject *)void_lbl;
-
-  for (int i = 0; !THREAD_STOP; i++) {
-    char s[100] = {0};
-    sprintf(s, "time since start %ds", i);
-    gtk_label_set_text(GTK_LABEL(lbl), s);
-    sleep(1);
-  }
-
-  return NULL;
-}
-
-void close_threads_and_quit(void)
-{
-
-  gtk_main_quit();
-
-  THREAD_STOP = 1;
-
-}
-
+#include "error.h"
 
 int main (int argc, char **argv)
 {
   GtkBuilder *builder;
-  GObject *window;
-  GObject *label;
   GError *error = NULL;
 
   gtk_init(&argc, &argv);
 
   builder = gtk_builder_new();
   if (gtk_builder_add_from_file(builder, "gui/main.ui", &error) == 0) {
-    g_printerr("Error loading file: %s\n", error->message);
+    timestamp("Error loading file: %s\n", error->message);
     g_clear_error(&error);
     return 1;
   }
