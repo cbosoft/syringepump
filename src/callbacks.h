250ec4b138101047b317ec38aa0e2d6d10dd534d
Chris <chrismjboyle@gmail.com>
2019-07-22 15:31:03 +0100

    Correct syntax for callback with data

--- src/callbacks.h
+++ src/callbacks.h
@@ -14,8 +14,8 @@ struct connect_data {
 
 struct connect_data *create_cd(int serial_fd, char *serial_path, int res, GObject *main_win, GObject *conn_btn, GObject *disconn_btn, GObject *log_lbl);
 
-void cb_connect(struct connect_data *cd);
-void cb_disconnect(struct connect_data *cd);
-void cb_quit(struct connect_data *cd);
+void cb_connect(GObject *obj, struct connect_data *cd);
+void cb_disconnect(GObject *obj, struct connect_data *cd);
+void cb_quit(GObject *obj, struct connect_data *cd);
 
 // vim: ft=c
