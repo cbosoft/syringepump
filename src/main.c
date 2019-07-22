f746cd18798fd3ba92dc6bb0c80f6a22fcebdd1c
Chris <chrismjboyle@gmail.com>
2019-07-22 10:31:16 +0100

    Added thread for updating gui, gui reader thing

--- src/main.c
+++ src/main.c
@@ -1,40 +1,61 @@
 #include <gtk/gtk.h>
+#include <pthread.h>
 
-static void print_hello(GtkWidget *widget, gpointer data)
+static int THREAD_STOP = 0;
+
+void *f(void * void_lbl)
 {
-  g_print("Hello, world!\n");
+
+  GObject *lbl = (GObject *)void_lbl;
+
+  for (int i = 0; !THREAD_STOP; i++) {
+    char s[100] = {0};
+    sprintf(s, "time since start %ds", i);
+    gtk_label_set_text(GTK_LABEL(lbl), s);
+    sleep(1);
+  }
+
+  return NULL;
 }
 
-static void activate (GtkApplication* app, gpointer user_data)
+void close_threads_and_quit(void)
 {
-  GtkWidget *window;
-  GtkWidget *button;
-  GtkWidget *button_box;
 
-  window = gtk_application_window_new (app);
-  gtk_window_set_title(GTK_WINDOW(window), "Window");
-  gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
+  gtk_main_quit();
 
-  button_box = gtk_button_box_new(GTK_ORIENTATION_HORIZONTAL);
-  gtk_container_add(GTK_CONTAINER(window), button_box);
+  THREAD_STOP = 1;
 
-  button = gtk_button_new_with_label("Hello, world!");
-  g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
-  g_signal_connect_swapped(button, "clicked", G_CALLBACK(gtk_widget_destroy), window);
-  gtk_container_add(GTK_CONTAINER(button_box), button);
-
-  gtk_widget_show_all(window);
 }
+
 
 int main (int argc, char **argv)
 {
-  GtkApplication *app;
-  int status;
+  GtkBuilder *builder;
+  GObject *window;
+  GObject *label;
+  GError *error = NULL;
 
-  app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
-  g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
-  status = g_application_run(G_APPLICATION(app), argc, argv);
-  g_object_unref (app);
+  gtk_init(&argc, &argv);
 
-  return status;
+  builder = gtk_builder_new();
+  if (gtk_builder_add_from_file(builder, "gui/main.ui", &error) == 0) {
+    g_printerr("Error loading file: %s\n", error->message);
+    g_clear_error(&error);
+    return 1;
+  }
+
+  window = gtk_builder_get_object(builder, "window");
+  g_signal_connect(window, "destroy", G_CALLBACK(close_threads_and_quit), NULL);
+
+  label = gtk_builder_get_object(builder, "label");
+
+  pthread_t thread;
+
+  pthread_create(&thread, NULL, f, label);
+
+  gtk_main();
+
+  pthread_join(thread, NULL);
+
+  return 0;
 }
