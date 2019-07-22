43f8ba2e8ed46f3f4bbb3cf0d1b7504531b8005a
Chris <chrismjboyle@gmail.com>
2019-07-22 15:32:02 +0100

    Log to stdout and gtklabel on data recv

--- src/log.c
+++ src/log.c
@@ -1,5 +1,6 @@
 #include "log.h"
 #include "ardiop.h"
+#include "error.h"
 
 extern int LOG_STOPPED;
 
@@ -8,22 +9,20 @@ void *log_update(void *vptr)
   struct thread_data *td = (struct thread_data *)vptr;
   // TODO open log file
   
-  int i = 0;
+  LOG_STOPPED = 0;
   while (!LOG_STOPPED) {
     char received_text[512] = {0};
     ard_readserial_line(td->serial_fd, received_text, 512, 1000);
+
+    timestamp("rec'd \"%s\"", received_text);
     
-    if (i % 10 == 0) {
-      // write to label
-      char *current_text = (char *)gtk_label_get_text(GTK_LABEL(td->log_lbl));
-      char *new_text = calloc(strlen(current_text) + strlen(received_text) + 1, sizeof(char));
-      strcat(new_text, current_text);
-      strcat(new_text, received_text);
-      gtk_label_set_text(GTK_LABEL(td->log_lbl), new_text);
-      free(new_text);
-      i = 0;
-    }
-    i ++;
+    // write to label
+    char *current_text = (char *)gtk_label_get_text(GTK_LABEL(td->log_lbl));
+    char *new_text = calloc(strlen(current_text) + strlen(received_text) + 1, sizeof(char));
+    strcat(new_text, current_text);
+    strcat(new_text, received_text);
+    gtk_label_set_text(GTK_LABEL(td->log_lbl), new_text);
+    free(new_text);
 
     // write to log file
     // TODO
