45c7f183a85e5d029fa9e160a844989710a8fcae
Chris <chrismjboyle@gmail.com>
2019-07-23 10:23:38 +0100

    Condensed setup

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <pthread.h>
 
 #include "error.h"
+#include "serial.h"
 #include "callbacks.h"
 
 int LOG_STOPPED = 1;
@@ -15,35 +16,31 @@ int main (int argc, char **argv)
   gtk_init(&argc, &argv);
 
   builder = gtk_builder_new();
-  if (gtk_builder_add_from_file(builder, "gui/main.ui", &error) == 0) {
-    timestamp("Error loading file: %s\n", error->message);
+  if (gtk_builder_add_from_file(builder, "gui/main2.ui", &error) == 0) {
+    timestamp(NULL, "Error loading file: %s\n", error->message);
     g_clear_error(&error);
     return 1;
   }
 
-  GObject *main_win     = gtk_builder_get_object(builder, "winMain");
-  GObject *log_lbl      = gtk_builder_get_object(builder, "lblLog");
-  GObject *close_btn    = gtk_builder_get_object(builder, "btnClose");
-  GObject *conn_btn     = gtk_builder_get_object(builder, "btnConn");
-  GObject *disconn_btn  = gtk_builder_get_object(builder, "btnDisconn");
-  GObject *ok_btn       = gtk_builder_get_object(builder, "btnOK");
-  GObject *scroll       = gtk_builder_get_object(builder, "scroll");
 
   struct connect_data *cd = create_cd(
       -1, 
       "/dev/ttyACM0",
       0,
-      main_win,
-      conn_btn,
-      disconn_btn,
-      log_lbl,
-      scroll);
+      gtk_builder_get_object(builder, "winMain"),
+      gtk_builder_get_object(builder, "btnConn"),
+      gtk_builder_get_object(builder, "btnDisconn"),
+      gtk_builder_get_object(builder, "btnSerialRefresh"),
+      gtk_builder_get_object(builder, "lblLog"),
+      gtk_builder_get_object(builder, "scroll"),
+      gtk_builder_get_object(builder, "cmbSerial"));
 
-  g_signal_connect(main_win, "destroy", G_CALLBACK(cb_quit), cd);
-  g_signal_connect(close_btn, "clicked", G_CALLBACK(cb_quit), cd);
-  g_signal_connect(ok_btn, "clicked", G_CALLBACK(cb_quit), cd);
-  g_signal_connect(conn_btn, "clicked", G_CALLBACK(cb_connect), cd);
-  g_signal_connect(disconn_btn, "clicked", G_CALLBACK(cb_disconnect), cd);
+  get_serial_name(GTK_COMBO_BOX_TEXT(cd->serial_cmb), GTK_WIDGET(cd->conn_btn));
+
+  g_signal_connect(cd->main_win, "destroy", G_CALLBACK(cb_quit), cd);
+  g_signal_connect(cd->conn_btn, "clicked", G_CALLBACK(cb_connect), cd);
+  g_signal_connect(cd->disconn_btn, "clicked", G_CALLBACK(cb_disconnect), cd);
+  g_signal_connect(cd->refresh_btn, "clicked", G_CALLBACK(cb_refresh_serial), cd);
 
   gtk_main();
 
