c7f2bd2f17618f2b12dba6ebe8abd4aa28a0e22b
Chris <chrismjboyle@gmail.com>
2019-07-23 17:43:33 +0100

    Added key-value pair passer func

--- src/serial.c
+++ src/serial.c
@@ -57,3 +57,47 @@ void refresh_serial_list(struct Data *data)
     }
   }
 }
+
+void send_key_value_to_arduino(struct Data *data, const char *key, void *val_vptr, int type)
+{
+  char value[(ARDUINO_MESG_LEN/2) + 1] = {0};
+
+  if (strlen(key) > ARDUINO_MESG_LEN/2) {
+    timestamp(data, 
+        "KEY TOO LARGE TO SEND (must be < %d chars, is %lu chars)", 
+        ARDUINO_MESG_LEN/2, 
+        strlen(key));
+    return;
+  }
+
+  switch(type) {
+    case T_STR:
+      if (strlen((char *)val_vptr) > ARDUINO_MESG_LEN/2) {
+        timestamp(data, 
+            "VALUE TOO LARGE TO SEND (must be < %d chars, is %lu chars)", 
+            ARDUINO_MESG_LEN/2,
+            strlen((char *)val_vptr));
+        return;
+      }
+      strcpy(value, (char*)val_vptr);
+      break;
+    case T_FLOAT:
+      snprintf(value, ARDUINO_MESG_LEN/2, "%f", CAST(val_vptr, float));
+      break;
+    case T_INT:
+      snprintf(value, ARDUINO_MESG_LEN/2, "%d", CAST(val_vptr, int));
+      break;
+  }
+
+  char mesg[ARDUINO_MESG_LEN+1] = {0};
+  strcat(mesg, key);
+  strcat(mesg, "=");
+  strcat(mesg, value);
+
+  timestamp(data, "sending k/v pair: %s", mesg);
+  write(data->serial_fd, mesg, ARDUINO_MESG_LEN);
+
+  // give the Arduino time to read the full string
+  sleep(2);
+
+}
--- src/serial.h
+++ src/serial.h
@@ -2,6 +2,13 @@
 #include <gtk/gtk.h>
 #include "data.h"
 
+#define CAST(VPTR, TYPE) (*(TYPE *)VPTR)
+#define T_STR 0
+#define T_FLOAT 1
+#define T_INT 2
+#define ARDUINO_MESG_LEN 64
+
 void refresh_serial_list(struct Data *data);
+void send_key_value_to_arduino(struct Data *data, const char *key, void *val_vptr, int type);
 
 // vim: ft=c
