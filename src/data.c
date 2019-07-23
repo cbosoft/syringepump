043a07fed298ace81f7d2544c9e76a0c4e8dca01
Chris <chrismjboyle@gmail.com>
2019-07-23 14:00:31 +0100

    Added input/entry boxes to data struct

--- src/data.c
+++ src/data.c
@@ -9,6 +9,10 @@ struct Data *new_data(
     GObject *conn_btn, 
     GObject *disconn_btn, 
     GObject *refresh_btn, 
+    GObject *setpoint_inp,
+    GObject *kp_inp,
+    GObject *ki_inp,
+    GObject *kd_inp,
     GObject *log_lbl,
     GObject *scroll,
     GObject *serial_cmb)
@@ -19,9 +23,18 @@ struct Data *new_data(
   data->serial_path = "/dev/ttyACM0";
   data->res = 0;
   data->main_win = main_win;
+  
+  // buttons
   data->conn_btn = conn_btn;
   data->disconn_btn = disconn_btn;
   data->refresh_btn = refresh_btn;
+
+  // inputs
+  data->setpoint_inp = setpoint_inp;
+  data->kp_inp = kp_inp;
+  data->ki_inp = ki_inp;
+  data->kd_inp = kd_inp;
+
   data->log_lbl = log_lbl;
   data->scroll = scroll;
   data->serial_cmb = serial_cmb;
