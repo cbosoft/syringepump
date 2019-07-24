8bd5ba9821baed67c7cccc20e8d26fc5330541b6
Chris <chrismjboyle@gmail.com>
2019-07-24 17:03:20 +0100

    Added calibration and script for position ruler

---
+++ data/proc.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+from matplotlib import pyplot as plt
+import numpy as np
+
+def read_csv(path):
+    with open(path) as csvf:
+        data = list(zip(*[[float(cell) for cell in l.split(',')] for l in csvf.readlines()[1:]]))
+    return data
+
+positions, readings = read_csv("cal.csv")
+coefs = np.polyfit(readings, positions, 1)
+print(coefs)
+fit_positions = np.add(np.multiply(coefs[0], readings), coefs[1])
+
+plt.plot(readings, positions, 'x')
+plt.plot(readings, fit_positions, '--')
+plt.show()
