a5f24d1da6d41a2327334824a5d7a7835f32b7eb
Chris <chrismjboyle@gmail.com>
2019-07-26 10:12:49 +0100

    Added logging to file

--- src/error.c
+++ src/error.c
@@ -3,9 +3,32 @@
 #include <string.h>
 #include <time.h>
 
+#ifndef WINDOWS
+#include <errno.h>
+#endif
+
 #include "error.h"
 #include "log.h"
 #include "data.h"
+
+#ifdef WINDOWS
+#error this file not yet ported (error.h)
+#endif
+
+
+const char *log_path = "log.txt";
+
+FILE *open_log(const char *path)
+{
+  FILE *fp = fopen(path, "a");
+
+  if (fp == NULL && errno == ENOENT)
+    fp = fopen(path, "w");
+
+  return fp;
+}
+
+
 
 void timestamp_error(struct Data *data, const char *fmt, ...)
 {
@@ -27,13 +50,34 @@ void timestamp_error(struct Data *data, const char *fmt, ...)
   strftime(timestr, MESGLEN-1, "%x %X", info);
 
   fprintf(stderr, "%s - %s\n", timestr, mesg);
+  FILE *logf = open_log(log_path);
+  fprintf(logf, "%s - %s\n", timestr, mesg);
+  fclose(logf);
+
 
   if (data != NULL) {
     char s[1000] = {0};
-    sprintf(s, "<tt><span foreground=\"white\" background=\"red\"><small>%s</small> <b>%s</b></span></tt>", timestr, mesg);
+    sprintf(s, 
+        "<tt>"
+        "<span foreground=\"white\" background=\"red\">"
+        "<small>%s</small> "
+        "<b>%s</b>"
+        "</span>"
+        "</tt>", timestr, mesg);
+    
+    if (errno) {
+      char e[1000] = {0};
+      sprintf(e, " (%d) %s", errno, strerror(errno));
+      strcat(s, e);
+      errno = 0;
+    }
+
     append_text_to_log(data, s);
   }
 }
+
+
+
 
 void timestamp(struct Data *data, const char *fmt, ...)
 {
@@ -55,6 +99,9 @@ void timestamp(struct Data *data, const char *fmt, ...)
   strftime(timestr, MESGLEN-1, "%x %X", info);
 
   fprintf(stderr, "%s - %s\n", timestr, mesg);
+  FILE *logf = open_log(log_path);
+  fprintf(logf, "%s - %s\n", timestr, mesg);
+  fclose(logf);
 
   if (data != NULL) {
     char s[1000] = {0};
