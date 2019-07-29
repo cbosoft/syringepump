7b6fab0f1f13600f430e85ae267022dfcf61c970
Chris <chrismjboyle@gmail.com>
2019-07-29 17:00:13 +0100

    Added calibration for optical encoder

--- spfw/optenc.h
+++ spfw/optenc.h
@@ -3,7 +3,7 @@
 
 #define OPT_PIN 2
 #define TIME_LEN 10
-#define TRIPS_PER_MM 1000.0
+#define TRIPS_PER_MM 367.6
 #define MS_PER_S 1000.0
 
 unsigned long getTripCount();
--- spfw/spfw.ino
+++ spfw/spfw.ino
@@ -8,12 +8,11 @@
 
 
 extern volatile unsigned long timer0_millis;
-bool STOPPED = false;
-bool REVERSING = false;
 unsigned long position = 0;
 double speed = 0.0;
-int control_action;
+int control_action = 0;
 long load_cell_reading = 0;
+void (*softReset)(void) = 0;
 
 
 
@@ -44,18 +43,23 @@ void setup ()
 
 void loop ()
 {
-  position = getTripCount();//getPositionReading();
+  position = getPositionReading();
   load_cell_reading = getLoadCellReading();
 
-  speed = 0;//getSpeedReading();
+  speed = getSpeedReading();
   control_action = getControlAction(speed);
   motorSetDC(control_action);
 
   logToSerial(load_cell_reading, position, speed);
 
-  while (getPositionReading() > RULER_POSITION_END) {
-    motorSetDC(0);
-    Serial.print("STOP\n");
-    delay(1000);
+  if (getPositionReading() > RULER_POSITION_END) {
+    for (int i = 0; i < 6; i++) {
+      motorSetDC(0);
+      Serial.print("STOP\n");
+      delay(500);
+    }
+
+    // after a few seconds of repeating "STOP", reset
+    softReset();
   }
 }
