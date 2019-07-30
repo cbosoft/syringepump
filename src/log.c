7dc7080252420366855c21dbc4599554feb302c6
Chris <chrismjboyle@gmail.com>
2019-07-30 17:15:11 +0100

    Moved log thread to log.c

--- src/log.c
+++ src/log.c
@@ -7,9 +7,119 @@
 #include "ardiop.h"
 #include "error.h"
 #include "data.h"
+#include "threads.h"
+#include "disconnect.h"
 
-extern int LOG_STOPPED;
+
+
+
+extern int log_worker_status;
 static char *log_string = NULL;
+static GThread *log_worker_thread;
+
+
+
+
+static void *log_worker(void *void_data)
+{ 
+  log_worker_status = THREAD_STARTED;
+  struct Data *data = (struct Data *)void_data;
+  const int print_every = 10;
+  
+  timestamp(data, "Waiting for Arduino...");
+  switch (wait_for(data, "START", 100, &log_worker_status, THREAD_CANCELLED)) {
+    case -1:
+      timestamp_error(data, "Cancelled by user.");
+      return NULL;
+    case -2:
+      timestamp_error(data, "Arduino connection timed out!");
+      return NULL;
+  }
+  timestamp(data, "Arduino ready, starting!");
+
+  get_new_log_name(data);
+  FILE *fp = fopen(data->logpath, "w");
+
+  int lineno = 0, charno = 0, timeout = 1000;
+  
+  struct timespec ms_span;
+  ms_span.tv_sec = 0;
+  ms_span.tv_nsec = 1000*1000;
+
+  while (log_worker_status < THREAD_CANCELLED) {
+    char received_text[512] = {0};
+    char b[1];
+    charno = 0;
+    do {
+      int n = read(data->serial_fd, b, 1);
+
+      if(n == -1) {
+        // error
+        timestamp_error(NULL, 
+            "something went wrong reading a byte (read failed) (%d) %s", 
+            errno, strerror(errno));
+        exit(1); // TODO handle properly
+      }
+      else if(n == 0) {
+        // no read
+        nanosleep(&ms_span, NULL);  // wait 1 msec try again
+        timeout--;
+        if(timeout == 0){
+          timestamp_error(NULL, 
+              "something went wrong reading a byte (timed out) (%d), %s", 
+              errno, strerror(errno));
+          exit(1); // TODO handle properly
+        }
+
+        continue;
+      }
+
+      received_text[charno] = b[0];
+
+      charno++;
+
+    } while(b[0] != '\n' && charno < 512 && (log_worker_status < THREAD_CANCELLED));
+
+    received_text[charno-1] = 0;
+
+    if (received_text[0] == 'P') {
+      timestamp(NULL, " :: %s", received_text);
+    }
+    else {
+      timestamp( 
+          ((lineno % print_every) == 0) ? data : NULL,
+          "R: %s",
+          received_text);
+      lineno = 
+        (lineno % print_every == 0) ? (1) : (lineno + 1);
+    }
+
+    if (strcmp(received_text, "STOP") == 0) {
+      // arduino requests stop
+      timestamp(data, "Arduino finished!");
+      log_worker_status = THREAD_STOPPED;
+    }
+    else {
+      // write to log file
+      fprintf(fp, "%s\n", received_text);
+    }
+
+  }
+
+  fclose(fp);
+
+  disconnect(data);
+
+  return NULL;
+}
+
+
+
+
+void start_log(struct Data *data)
+{
+  log_worker_thread = g_thread_new("log_thread", log_worker, data);
+}
 
 
 
--- src/log.h
+++ src/log.h
@@ -1,9 +1,9 @@
 #pragma once
-#include <gtk/gtk.h>
 
 #include "data.h"
 
 void append_text_to_log(struct Data *data, const char *added_text);
 char *get_new_log_name(struct Data *data);
+void start_log(struct Data *data);
 
 // vim: ft=c
