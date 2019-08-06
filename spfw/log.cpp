2a8e4181b4b910801a86e54ee93852fde8ce4e1d
Chris <chrismjboyle@gmail.com>
2019-08-06 13:30:38 +0100

    Added title printer

--- spfw/log.cpp
+++ spfw/log.cpp
@@ -1,26 +1,38 @@
 #include <Arduino.h>
 #include "log.h"
 
-void logToSerial(long load_cell_reading, unsigned long position, double speed, double control_action) {
+void logToSerial(long time, double load_cell_reading, double position, double speed)
+{
   // time since start
-  Serial.print(millis());
+  Serial.print(time);
   Serial.print(",");
 
   // load cell
-  Serial.print(load_cell_reading);
+  Serial.print(load_cell_reading, 5);
   Serial.print(",");
 
   // position (ruler)
-  Serial.print(position);
+  Serial.print(position, 5);
   Serial.print(",");
 
   // speed
   Serial.print(speed, 5);
-  Serial.print(",");
-
-  // control action
-  Serial.print(control_action, 5);
   Serial.print("\n");
+}
+
+void logTitlesToSerial()
+{
+  // time since start
+  Serial.print("Time (ms),");
+
+  // load cell
+  Serial.print("Load Cell (N),");
+
+  // position (ruler)
+  Serial.print("Distance to end (mm),");
+
+  // speed
+  Serial.print("Flowrate (ml/s)\n");
 }
 
 // vim: ft=arduino
--- spfw/log.h
+++ spfw/log.h
@@ -1,7 +1,8 @@
 #ifndef __LOG__
 #define __LOG__
 
-void logToSerial(long load_cell_reading, unsigned long position, double speed, double control_action);
+void logToSerial(long time, double load_cell_reading, double position, double speed);
+void logTitlesToSerial();
 
 #endif
 
