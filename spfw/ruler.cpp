675ef49e53e3dea5392b57ad3fd23e009bbadd24
Chris <chrismjboyle@gmail.com>
2019-08-06 13:30:58 +0100

    Added diameter (units) getter

--- spfw/ruler.cpp
+++ spfw/ruler.cpp
@@ -1,12 +1,53 @@
 #include <Arduino.h>
 #include "ruler.h"
 
-double getPositionReadingMillimeter()
+
+const double diameters[10] = {5, 10, 20, 30, 40};
+
+
+double getInternalDiameterUnits()
 {
-  double byte_pos_f = double(getPositionReading());
+  // TODO
+
+  return 1.0;
+}
+
+
+
+
+double getDiameterReadingUnits()
+{
+  return convertDiameterReadingUnits(getDiameterReading());
+}
+
+
+
+
+double convertDiameterReadingUnits(unsigned long diameter_nounits)
+{
+  double byte_dia_f = double(diameter_nounits);
+
+  return (byte_dia_f * RULER_DIAMETER_CAL_M) + RULER_DIAMETER_CAL_C;
+}
+
+
+
+
+double getPositionReadingUnits()
+{
+  return convertPositionReadingUnits(getPositionReading());
+}
+
+
+
+
+double convertPositionReadingUnits(unsigned long position_nounits)
+{
+  double byte_pos_f = double(position_nounits);
 
   return (byte_pos_f * RULER_POSITION_CAL_M) + RULER_POSITION_CAL_C;
 }
+
 
 
 
--- spfw/ruler.h
+++ spfw/ruler.h
@@ -2,15 +2,23 @@
 #define __RULER__
 
 #define RULER_POSITION_PIN A0
-#define RULER_DIAMETER_PIN A1
 #define RULER_POSITION_END 960
 #define RULER_POSITION_START 0
 #define RULER_POSITION_CAL_M -0.12646004
 #define RULER_POSITION_CAL_C 124.12431658
 
+#define RULER_DIAMETER_PIN A1
+#define RULER_DIAMETER_CAL_M 1.0
+#define RULER_DIAMETER_CAL_C 0.0
+
 unsigned long getPositionReading();
 unsigned long getDiameterReading();
-double getPositionReadingMillimeter();
+
+double convertPositionReadingUnits(unsigned long position_nounits);
+double convertDiameterReadingUnits(unsigned long diameter_nounits);
+double getPositionReadingUnits();
+double getDiameterReadingUnits();
+double getInternalDiameterUnits();
 
 #endif
 
