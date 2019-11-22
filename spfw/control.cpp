5faa5f6d57644589321b92151b344d776c13fb0c
Chris <chrismjboyle@gmail.com>
2019-11-22 16:58:46 +0000

    Simplified param sending

--- spfw/control.cpp
+++ spfw/control.cpp
@@ -16,7 +16,7 @@ enum controllers {
 };
 int control_type = CONTROL_UNSET;
 int controlled_var = 0;
-int raw_lc = 0;
+int log_options = 56; // 111000
 double err1 = 0.0;
 double err2 = 0.0;
 
@@ -75,11 +75,17 @@ double getControlAction(double pca, double speed, double force)
 
 void controlInit(){
 
-  int params_got = 0;
-  int params_needed = 1;
+  int done = 0,
+    sp_recvd = 0,
+    tp_recvd = 0,
+    dc_recvd = 0,
+    bf_recvd = 0,
+    di_recvd = 0,
+    lo_recvd = 0;
+
   Serial.print("WAIT\n");
 
-  while (params_got < params_needed) {
+  for (int i = 0; i < 100 && !done; i++) {
 
     while (!Serial.available()) delay(100);
 
@@ -91,54 +97,74 @@ void controlInit(){
 
     if (key == 0) continue;
 
-    if (strcmp(key, "setpoint") == 0) {
+    if (strcmp(key, "SP") == 0) {
       control_type = CONTROL_PID;
+
+      controlled_var = val[0] != 'F';
+      val++;
+
       setpoint = atof(val);
+
+      sp_recvd = 1;
     }
-    else if (strcmp(key, "var") == 0) {
+    else if (strcmp(key, "TP") == 0) {
       control_type = CONTROL_PID;
-      controlled_var = strcmp(val, "force") != 0;
+
+      char *param = strtok(val, ",");
+      kp = atof(param);
+      param = strtok(0, ",");
+      ki = atof(param);
+      param = strtok(0, ",");
+      kd = atof(param);
+
+      tp_recvd = 1;
     }
-    else if (strcmp(key, "kp") == 0) {
-      control_type = CONTROL_PID;
-      kp = atof(val);
-    }
-    else if (strcmp(key, "ki") == 0) {
-      control_type = CONTROL_PID;
-      ki = atof(val);
-    }
-    else if (strcmp(key, "kd") == 0) {
-      control_type = CONTROL_PID;
-      kd = atof(val);
-    }
-    else if (strcmp(key, "dc") == 0) {
+    else if (strcmp(key, "DC") == 0) {
       control_type = CONTROL_NONE;
       dc = atof(val);
-    }
-    else if (strcmp(key, "bl") == 0) {
-      buflen = atof(val);
-    }
-    else if (strcmp(key, "di") == 0) {
-      inner_diameter = atof(val);
-    }
-    else if (strcmp(key, "raw") == 0) {
-      raw_lc = atoi(val);
-    }
 
-    params_got ++;
+      dc_recvd = 1;
+    }
+    else if (strcmp(key, "BF") == 0) {
+      buflen = atof(val);
+
+      bf_recvd = 1;
+    }
+    else if (strcmp(key, "DI") == 0) {
+      inner_diameter = atof(val);
+
+      di_recvd = 1;
+    }
+    else if (strcmp(key, "LO") == 0) {
+      log_options = atoi(val);
+
+      lo_recvd = 1;
+    }
 
     switch (control_type) {
-      case CONTROL_PID:
-        params_needed = 8;
-        break;
-      case CONTROL_UNSET:
-      case CONTROL_NONE:
-        params_needed = 4;
-        break;
+
+    case CONTROL_PID:
+      if (sp_recvd && tp_recvd && lo_recvd && di_recvd && bf_recvd)
+        done = 1;
+      break;
+
+    case CONTROL_NONE:
+      if (dc_recvd && lo_recvd && di_recvd && bf_recvd)
+        done = 1;
+      break;
+
     }
+
     Serial.print("OK\n");
 
   } 
+
+  if (!done) {
+    while (1) {
+      Serial.print("STOP\n");
+      Serial.print("Incorrect parameter set given.\n");
+    }
+  }
 }
 
 // vim: ft=arduino
