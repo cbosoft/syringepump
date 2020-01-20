ee0d63525a10ff00472bff5fd97f39dfcc174bff
Chris <chrismjboyle@gmail.com>
2020-01-20 10:55:12 +0000

    Add classes to syrpu/__init__.py
    Eases importing

---
+++ python/syrpu/__init__.py
@@ -0,0 +1,5 @@
+from syrpu.syringepump import Syringepump
+from syrpu.controller import PIDController, PassiveController
+from syrpu.setter import ConstantSetter
+from syrpu.logoptions import LogOptions
+from syrpu.geometryoptions import GeometryOptions
