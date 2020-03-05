6b508c09123e753eae9d8d3a3cb5677c2277d832
Chris <chrismjboyle@gmail.com>
2020-03-05 11:47:17 +0000

    Print error to status box; manual switch to show errno

--- src/error.c
+++ src/error.c
@@ -1,3 +1,4 @@
+#define _POSIX_C_SOURCE 200809L
 #include <stdlib.h>
 #include <stdarg.h>
 #include <stdio.h>
@@ -11,6 +12,7 @@
 #include "error.h"
 #include "log.h"
 #include "data.h"
+#include "form.h"
 
 #ifdef WINDOWS
 #error this file not yet ported (error.h)
@@ -39,6 +41,19 @@ gboolean timestamp_callback(struct timestamp_callback_data *tcd)
 {
 
   append_text_to_log(tcd->data, tcd->message);
+
+  free(tcd->message);
+  free(tcd);
+
+  return 0;
+}
+
+gboolean status_callback(struct timestamp_callback_data *tcd)
+{
+
+  GObject *status_label = get_object_safe(tcd->data, "lblStatus");
+  gtk_label_set_markup(GTK_LABEL(status_label), tcd->message);
+
   free(tcd->message);
   free(tcd);
 
@@ -47,8 +62,9 @@ gboolean timestamp_callback(struct timestamp_callback_data *tcd)
 
 
 
-void timestamp_error(struct Data *data, int is_gui, const char *fmt, ...)
+void timestamp_error(struct Data *data, int is_gui, int show_errno, const char *fmt, ...)
 {
+  (void) is_gui;
 
   char mesg[MESGLEN] = {0};
 
@@ -67,38 +83,36 @@ void timestamp_error(struct Data *data, int is_gui, const char *fmt, ...)
   strftime(timestr, MESGLEN-1, "%x %X", info);
 
   fprintf(stderr, "%s - %s\n", timestr, mesg);
-  FILE *logf = open_log(log_path);
-  fprintf(logf, "%s - %s\n", timestr, mesg);
-  fclose(logf);
-
+  //FILE *logf = open_log(log_path);
+  //fprintf(logf, "%s - %s\n", timestr, mesg);
+  //fclose(logf);
 
   if (data != NULL) {
-    char s[1000] = {0};
-    sprintf(s, 
+    char s[ERROR_BUFFER_SIZE+1] = {0};
+    snprintf(s, ERROR_BUFFER_SIZE,
         "<tt>"
         "<span foreground=\"white\" background=\"red\">"
-        "<small>%s</small> "
         "<b>%s</b>"
         "</span>"
-        "</tt>", timestr, mesg);
+        "</tt>", mesg);
     
-    if (errno && errno != 11) {
+    if (show_errno && errno) {
 
-      char e[1000] = {0};
-      sprintf(e, " (%d) %s", errno, strerror(errno));
-      strcat(s, e);
+      char e[(ERROR_BUFFER_SIZE/2)+1] = {0};
+      snprintf(e, ERROR_BUFFER_SIZE/2, " (%d) %s", errno, strerror(errno));
+      strncat(s, e, ERROR_BUFFER_SIZE);
       errno = 0;
 
     }
     
+
+    struct timestamp_callback_data *tcd = calloc(1, sizeof(struct timestamp_callback_data));
+    tcd->data = data;
+    tcd->message = strdup(s);
     if (is_gui)
-      append_text_to_log(data, s);
+      status_callback(tcd);
     else {
-      struct timestamp_callback_data *tcd = calloc(1, sizeof(struct timestamp_callback_data));
-      tcd->data = data;
-      tcd->message = calloc(1+strlen(s), sizeof(char));
-      strcpy(tcd->message, s);
-      g_idle_add((GSourceFunc)timestamp_callback, tcd);
+      g_idle_add((GSourceFunc)status_callback, tcd);
     }
   }
 }
@@ -109,6 +123,8 @@ void timestamp_error(struct Data *data, int is_gui, const char *fmt, ...)
 void timestamp(struct Data *data, int is_gui, const char *fmt, ...)
 {
 
+  (void) is_gui;
+
   char mesg[MESGLEN] = {0};
 
   va_list ap;
@@ -131,17 +147,16 @@ void timestamp(struct Data *data, int is_gui, const char *fmt, ...)
   fclose(logf);
 
   if (data != NULL) {
-    char s[1000] = {0};
-    sprintf(s, "<tt><small>%s <b>%s</b></small></tt>", timestr, mesg);
+    char s[ERROR_BUFFER_SIZE] = {0};
+    snprintf(s, ERROR_BUFFER_SIZE, "<tt><small>%s <b>%s</b></small></tt>", timestr, mesg);
 
-    if (is_gui)
-      append_text_to_log(data, s);
-    else {
-      struct timestamp_callback_data *tcd = calloc(1, sizeof(struct timestamp_callback_data));
-      tcd->data = data;
-      tcd->message = calloc(1+strlen(s), sizeof(char));
-      strcpy(tcd->message, s);
-      g_idle_add((GSourceFunc)timestamp_callback, tcd);
-    }
+    // if (is_gui)
+    //   append_text_to_log(data, s);
+    // else {
+    //   struct timestamp_callback_data *tcd = calloc(1, sizeof(struct timestamp_callback_data));
+    //   tcd->data = data;
+    //   tcd->message = strdup(s);
+    //   g_idle_add((GSourceFunc)timestamp_callback, tcd);
+    // }
   }
 }
--- src/error.h
+++ src/error.h
@@ -1,10 +1,12 @@
 #pragma once
 
 #define MESGLEN 256
+#define ERROR_BUFFER_SIZE 1000
 
 #include "data.h"
 
-void timestamp_error(struct Data *data, int is_gui, const char *fmt, ...);
+
+void timestamp_error(struct Data *data, int is_gui, int show_errno, const char *fmt, ...);
 void timestamp(struct Data *data, int is_gui, const char *fmt, ...);
 
 // vim: ft=c
